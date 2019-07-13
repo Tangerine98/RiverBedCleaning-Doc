6af97827b93599f852fd62a4530a552cb675b844
Tangerine98 <menonnandini98@gmail.com>
2019-07-13 13:05:39 +0530

    Initial Commit

---
+++ assets/bootstrap_002.js
@@ -0,0 +1,18 @@
+<html>
+<head><title>403 Forbidden</title></head>
+<body>
+<h1>403 Forbidden</h1>
+<ul>
+<li>Code: AccessDenied</li>
+<li>Message: Access Denied</li>
+<li>RequestId: F178C0CA6118D440</li>
+<li>HostId: KmeWp63gMZpg3haHxEElgfAMwkd69dDy5c45lC8O6BRZObM5xlKUNz2rcJyhFF3mmp3O1c8ULHY=</li>
+</ul>
+<h3>An Error Occurred While Attempting to Retrieve a Custom Error Document</h3>
+<ul>
+<li>Code: AccessDenied</li>
+<li>Message: Access Denied</li>
+</ul>
+<hr/>
+</body>
+</html>
---
+++ assets/jquery-1.js
@@ -0,0 +1,18 @@
+<html>
+<head><title>403 Forbidden</title></head>
+<body>
+<h1>403 Forbidden</h1>
+<ul>
+<li>Code: AccessDenied</li>
+<li>Message: Access Denied</li>
+<li>RequestId: EA49682AD76F4711</li>
+<li>HostId: zg6hYxto2qSTemlOUV22vd1Cw4+tOX34f2XaOp1qg2vMJCviDZrfVk80EIfscp+Vc/Rv6lX/53I=</li>
+</ul>
+<h3>An Error Occurred While Attempting to Retrieve a Custom Error Document</h3>
+<ul>
+<li>Code: AccessDenied</li>
+<li>Message: Access Denied</li>
+</ul>
+<hr/>
+</body>
+</html>
